60d80a7c3afea93b9ef9fa6cc772ca848e1a25b6
Alberto Acosta <alberto.acosta@live.com>
2020-06-28 23:38:31 +0100

    FIX: unit testing

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,16 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import 'react-native';
 import React from 'react';
-import App from '../App';
+import { App } from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, { act } from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  act(() => {
+    renderer.create(<App />);
+  });
 });
